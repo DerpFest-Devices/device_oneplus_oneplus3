e792162e34dc8e72831cf489c9a837452cdc599c
Gaurav241 <gaurav.govinda2420@gmail.com>
2020-03-09 20:28:02 +0530

    op3: DeviceParts: Remove Panel Mode

--- DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -51,14 +51,9 @@ public class DeviceSettings extends PreferenceFragment implements
     private static final String KEY_BUTTON_CATEGORY = "buttons_category";
     private static final String KEY_CATEGORY_GRAPHICS = "graphics";
 
-    public static final String KEY_SRGB_SWITCH = "srgb";
     public static final String KEY_HBM_SWITCH = "hbm";
     public static final String KEY_HWK_SWITCH = "hwk";
     public static final String KEY_PROXI_SWITCH = "proxi";
-    public static final String KEY_DCI_SWITCH = "dci";
-    public static final String KEY_NIGHT_SWITCH = "night";
-    public static final String KEY_ADAPTIVE_SWITCH = "adaptive";
-    public static final String KEY_ONEPLUS_SWITCH = "oneplus";
 
     public static final String SLIDER_DEFAULT_VALUE = "2,1,0";
 
